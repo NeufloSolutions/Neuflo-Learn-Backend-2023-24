cd83615cb5eae3b8063284bc9805429f11c98888
Milan <milan.varghese@neuflo.io>
2024-02-17 18:59:51 +0000

    updated mocktest code, added target score setting api

--- Backend/DBConfig/db_connection.py
+++ Backend/DBConfig/db_connection.py
@@ -1,8 +1,8 @@
 # db_connection.py
 import psycopg2
 from psycopg2 import pool
-import redis
-from Backend.dbconfig.config import DB_CONFIG, REDIS_CONFIG
+# import redis
+from Backend.dbconfig.config import DB_CONFIG
 
 # Initialize the connection pool for PostgreSQL
 def init_pg_connection_pool():
@@ -12,20 +12,20 @@ def init_pg_connection_pool():
     return connection_pool
 
 # Initialize Redis client
-def init_redis_client():
-    # Replace these with your Azure Redis configuration details
-    azure_redis_host = REDIS_CONFIG['azure_redis_host']
-    azure_redis_port = REDIS_CONFIG['azure_redis_port']  # Default port for Azure Redis with SSL
-    azure_redis_password = REDIS_CONFIG['azure_redis_password']
+# def init_redis_client():
+#     # Replace these with your Azure Redis configuration details
+#     azure_redis_host = REDIS_CONFIG['azure_redis_host']
+#     azure_redis_port = REDIS_CONFIG['azure_redis_port']  # Default port for Azure Redis with SSL
+#     azure_redis_password = REDIS_CONFIG['azure_redis_password']
 
-    return redis.StrictRedis(
-        host=azure_redis_host, 
-        port=azure_redis_port, 
-        password=azure_redis_password, 
-        db=0, 
-        ssl=True, 
-        ssl_cert_reqs=None
-    )
+#     return redis.StrictRedis(
+#         host=azure_redis_host, 
+#         port=azure_redis_port, 
+#         password=azure_redis_password, 
+#         db=0, 
+#         ssl=True, 
+#         ssl_cert_reqs=None
+#     )
 # Function to create and return a new PostgreSQL connection
 def create_pg_connection(connection_pool):
     try:
@@ -41,4 +41,4 @@ def release_pg_connection(connection_pool, connection):
 
 # Initialize connection pools and clients
 pg_connection_pool = init_pg_connection_pool()
-redis_client = init_redis_client()
+# redis_client = init_redis_client()
